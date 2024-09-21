514585be95d414b4c7ebd68cebf3619696f67036
joeyirish819 <joannemuldowney819@gmail.com>
2024-09-21 18:41:57 +0100

    added Inter font family to app

--- src/src/index.js
+++ src/src/index.js
@@ -53,3 +53,26 @@ let currentDateElement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateElement.innerHTML = formatDate(currentDate);
+
+// const apiKey = "94a0dd46893159096ot0afa98ccebd36";
+
+// function fetchWeatherData(city) {
+//   const url = `https://api.shecodes.io/weather/v1/current?query=${encodeURIComponent(
+//     city
+//   )}&units=metric&appid=${apiKey}`;
+
+//   axios
+//     .get(url)
+//     .then((response) => {
+//       const cityName = response.data.city.name;
+//       const temp = response.data.main.temp;
+
+//       document.getElementById("city").textContent = cityName;
+//       document.getElementById("temp").textContent = `${temp}°C`;
+//     })
+//     .catch((error) => {
+//       console.error("Fetch error:", error);
+//       document.getElementById("error-message").textContent =
+//         "Error fetching data. Please try again.";
+//     });
+// }
