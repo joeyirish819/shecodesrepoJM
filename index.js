60348853705161217e970bf90fecc328f532d60f
joeyirish819 <joannemuldowney819@gmail.com>
2024-10-02 16:52:58 +0100

    removed weather html and added js loop and array for forecast

--- index.js
+++ index.js
@@ -52,6 +52,29 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecast = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  days.forEach(function(day) {
+
+  });
+  
+forecast.innerHTML=`
+<div class="weather-forecast-day">
+            <div class="weather-forecast-date">Tue</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+              <strong>15°</strong> 
+            </div>
+            <div class="weather-forecast-temperature">9°</div>
+        </div>
+      </div>
+      `;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
